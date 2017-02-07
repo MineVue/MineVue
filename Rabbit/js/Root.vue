854347a16e29d73afac0f9e7bcf81c5a04a46385
BaeCheol Min <cjfals0904@gmail.com>
2017-02-07 23:33:45 +0900

    RootVue에 스타일 추가, 부트스트랩 link, script 추가

--- Rabbit/js/Root.vue
+++ Rabbit/js/Root.vue
@@ -1,5 +1,74 @@
 <template>
-    <div id="root">
-        <router-view></router-view>
+    <div class="container row" id="root">
+       <div class="col-md-2 col-lg-2 left-side">
+            <nav>
+                <ul class="nav sidebar-nav">
+                    <li>
+                        <a href="#">
+                            HOME
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            번호내역
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            가입현황
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            웹 사용현황
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            모바일 사용 현황
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+       </div>
+       <div class="col-md-12 col-lg-12  contents-wrapper">
+            <router-view></router-view>
+       </div>
     </div>
 </template>
+
+<style>
+    html, body, .container {height: 100%;}
+    .container {
+        display:table;
+        width:100%;
+        margin: 0;
+        padding: 0;
+    }
+    .container .left-side {
+        display:table-cell;
+        float: none;
+        background: #000;
+        padding:0;
+    }
+    .container .content-wrapper {
+        display: table-cell;
+        float: none;
+    }
+    .container .left-side a {
+        color: #BDBDBD;
+        text-align: center;
+        padding: 15px 0;
+        text-decoration: none;
+    }
+    .container .left-side a:hover {
+        background: rgba(255, 255, 255, 0.2);
+    }
+    .container .contents-wrapper {
+        background: #ECEFF1;
+        height:100%;
+    }
+    a:active, a:visited {
+        background: #000;
+    }
+</style>
--- Rabbit/webpack.config.js
+++ Rabbit/webpack.config.js
@@ -14,33 +14,56 @@ var common = {
         publicPath: '/dist/',
         filename: 'bundle.js'
     },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: 'jquery'
+        })
+    ],
     module: {
-        rules: [{
-            test: /\.vue$/,
-            loader: 'vue-loader',
-            options: {
-                loaders: {
-                    // Since sass-loader (weirdly) has SCSS as its default parse mode, we map
-                    // the "scss" and "sass" values for the lang attribute to the right configs here.
-                    // other preprocessors should work out of the box, no loader config like this nessessary.
-                    'scss': 'vue-style-loader!css-loader!sass-loader',
-                    'sass': 'vue-style-loader!css-loader!sass-loader?indentedSyntax'
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader',
+                options: {
+                    loaders: {
+                        // Since sass-loader (weirdly) has SCSS as its default parse mode, we map
+                        // the "scss" and "sass" values for the lang attribute to the right configs here.
+                        // other preprocessors should work out of the box, no loader config like this nessessary.
+                        'scss': 'vue-style-loader!css-loader!sass-loader',
+                        'sass': 'vue-style-loader!css-loader!sass-loader?indentedSyntax'
+                    }
                 }
+            }, {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015']
+                }
+            }, {
+                test: /\.(png|jpg|gif|svg)$/,
+                loader: 'file-loader',
+                options: {
+                  name: '[name].[ext]?[hash]'
+                }
+            },
+            {
+                test: /\.(woff|woff2)$/,
+                loader: "url-loader?limit=10000&mimetype=application/font-woff"
+            },
+            {
+                test: /\.ttf$/,
+                loader: "file-loader"
+            },
+            { 
+                test: /\.eot$/,
+                loader: "file-loader" 
+            },
+            { 
+                test: /\.svg$/,
+                loader: "file-loader" 
             }
-        }, {
-            test: /\.js$/,
-            exclude: /node_modules/,
-            loader: 'babel-loader',
-            query: {
-                presets: ['es2015']
-            }
-        }, {
-            test: /\.(png|jpg|gif|svg)$/,
-            loader: 'file-loader',
-            options: {
-              name: '[name].[ext]?[hash]'
-            }
-        }]
+        ]
     },
     resolve: {
         modules: [path.resolve(__dirname, "./src"), path.resolve(__dirname, "./node_modules")],
