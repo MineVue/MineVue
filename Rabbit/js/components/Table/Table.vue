0f4219472b958634606e880967239824575b51d0
BaeCheol Min <cjfals0904@gmail.com>
2017-02-14 00:04:38 +0900

    menu.json 추가

---
+++ Rabbit/js/components/Table/Table.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="panel panel-default">
+        <div class="panel-heading">
+        </div>
+        <table class="table">
+            <tr>
+                <th></th>
+                <th></th>
+                <th></th>
+                <th></th>
+                <th></th>
+                <th></th>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+        </table>
+    </div>
+</template>
+<script src="./Table.js"></script>
+<style src="./Table.css"></style>
