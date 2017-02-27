ba13524dcf62413482c59b0ada0d2a1951ea2e61
cjfals0904 <dev_bcm@sk.com>
2017-02-27 21:50:13 +0900

    전반적인 스타일링 수정

--- Rabbit/js/reducers/main.js
+++ Rabbit/js/reducers/main.js
@@ -9,9 +9,12 @@ export default function main(state = INITIAL_STATE, action) {
   switch (action.type) {
     case MAIN_ACTION.FETCH_MENU_LIST_END :
         const list = state.list
-        for (let i = 0; i < action.list.length; i++) {
-            list.push(action.list[i]);
+        if (list.length === 0) {
+          for (let i = 0; i < action.list.length; i++) {
+              list.push(action.list[i]);
+          }
         }
+
         return Object.assign({}, state, {
             list
         });
