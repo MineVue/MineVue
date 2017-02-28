6ebd3744f0e37d9a1fdbd53aef0716984c0f6f64
kazikai <kazikai84@gmail.com>
2017-02-28 18:40:23 +0900

    initial commit

---
+++ Mouse-kazikai/src/app.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="main">
+    <input type="text" v-model="text" />
+    <div class="hello">Hello {{ text }}!</div>
+  </div>
+</template>
+
+<script>
+  export default {
+    replace: false,
+  	data() {
+      return {
+        text: 'vue'
+      }
+    }
+  }
+</script>
